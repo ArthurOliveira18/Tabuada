f303b526766c70c3af8baaabd1ffe6a2d326cd0d
Arthur Evangelista Oliveira <arturevangelistaoliveira@gmail.com>
2025-05-17 17:25:25 -0300

    Add comentários

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ const multiplicationTable = document.getElementById("multiplication-operations")
 // Funções
 const createTable = (number, multiplicatorNumber) => {
 
-    //Limpando o HTML ao iniciar
+
     const operationsDiv = document.getElementById("multiplication-operations")
-    operationsDiv.innerHTML = "" // Limpa apenas as operações, mantendo o título
+    operationsDiv.innerHTML = ""
 
     for (let i = 1; i <= multiplicatorNumber; i++) {
 
@@ -20,12 +20,20 @@ const createTable = (number, multiplicatorNumber) => {
 
         const template =
             `<div class = "row">
-            <div class = "operations"> ${number} X ${i} = </div>
-            <div class = "result"> ${result} </div>
-        </div>
-        `
+                <div class = "operations"> ${number} X ${i} = </div>
+                <div class = "result"> ${result} </div>
+            </div>`
+
+
+        //Cria um objeto que pode analisar (parsear) strings contendo HTML/XML e convertê-las em um documento DOM.    
         const parser = new DOMParser()
+
+        //Converte a string HTML (template) em um documento HTML real (não apenas texto).
+        //O segundo argumento ("text/html") especifica que o conteúdo deve ser interpretado como HTML.
         const htmlTemplate = parser.parseFromString(template, "text/html")
+
+        //Busca dentro desse documento HTML temporário um elemento com a classe row.
+        //Retorna o primeiro elemento que corresponde ao seletor CSS (no caso, a div.row que você criou no template).
         const row = htmlTemplate.querySelector(".row")
 
         multiplicationTable.appendChild(row)
@@ -38,7 +46,7 @@ const createTable = (number, multiplicatorNumber) => {
 multiplicationForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    
+
     const number = +numberInput.value
     const multiplicatorNumber = +multiplicatorInput.value
 
