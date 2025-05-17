e1759de17099445c7bb5868be7cffe09b75f42a0
Arthur Evangelista Oliveira <arturevangelistaoliveira@gmail.com>
2025-05-17 17:22:07 -0300

    Finalizado a tabuada

--- js/script.js
+++ js/script.js
@@ -1,23 +1,54 @@
-//Seleção de elementos
 const multiplicationForm = document.getElementById("multiplication-form")
 const numberInput = document.getElementById("number")
 const multiplicatorInput = document.getElementById("multiplicator")
-// Funções
+const multiplicationTable = document.getElementById("multiplication-operations")
 
+
+
+// Funções
+const createTable = (number, multiplicatorNumber) => {
+
+    //Limpando o HTML ao iniciar
+    const operationsDiv = document.getElementById("multiplication-operations")
+    operationsDiv.innerHTML = "" // Limpa apenas as operações, mantendo o título
+
+    for (let i = 1; i <= multiplicatorNumber; i++) {
+
+        const result = number * i
+
+        console.log(result)
+
+        const template =
+            `<div class = "row">
+            <div class = "operations"> ${number} X ${i} = </div>
+            <div class = "result"> ${result} </div>
+        </div>
+        `
+        const parser = new DOMParser()
+        const htmlTemplate = parser.parseFromString(template, "text/html")
+        const row = htmlTemplate.querySelector(".row")
+
+        multiplicationTable.appendChild(row)
+    }
+
+
+}
 
 //Eventos
-multiplicationForm.addEventListener("submit" , (e)=>{
+multiplicationForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    const multiplicationNumber = +numberInput.value
+    
+    const number = +numberInput.value
     const multiplicatorNumber = +multiplicatorInput.value
 
     //Verificação para o usuário colocar os dois números
-    if(!multiplicationNumber || !multiplicatorNumber){
+    if (!number || !multiplicatorNumber) {
 
         //Não retorna nada pois não terá nada para retornar
         return
     }
 
-    console.log(multiplicationNumber,multiplicatorNumber)
+    createTable(number, multiplicatorNumber)
+
 })
